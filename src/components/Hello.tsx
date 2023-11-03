25d4708f8dff23c863827507e03a5ee361e47717
batako <guri725ren213@gmail.com>
2023-11-03 17:38:46 +0900

    Hello コンポーネントのスタイルとpropsの設定

--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -1,9 +1,21 @@
 import { View, Text, StyleSheet } from 'react-native'
 
-const Hello = (): JSX.Element => {
+interface Props {
+  children: string
+  showMark?: boolean
+}
+
+const Hello = (props: Props): JSX.Element => {
+  const {
+    children,
+    showMark = false,
+  } = props
+
   return (
     <View>
-      <Text style={styles.text}>Hello</Text>
+      <Text style={styles.text}>
+        Hello {children}{showMark && '!'}
+      </Text>
     </View>
   )
 }
