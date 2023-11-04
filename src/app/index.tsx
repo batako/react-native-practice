6005d18d595520187782ad6eb942db123aa32a1f
batako <guri725ren213@gmail.com>
2023-11-04 11:57:57 +0900

    メモ一覧のページをトップのファイルから移動

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,27 +1,7 @@
-import { StyleSheet, View } from 'react-native'
-
-import CircleButton from '../components/CircleButton'
-import Header from '../components/Header'
-import Memos from '../components/Memos'
+import { Redirect } from 'expo-router'
 
 const Index = (): JSX.Element => {
-  return (
-    <View style={styles.container}>
-      <Header />
-
-      <Memos />
-
-      <CircleButton>+</CircleButton>
-    </View>
-  )
+  return <Redirect href='memo/list' />
 }
-
-const styles = StyleSheet.create({
-  container: {
-    // 要素を画面全体に広げる
-    flex: 1,
-    backgroundColor: '#ffffff',
-  },
-})
 
 export default Index
---
+++ src/app/memo/list.tsx
@@ -0,0 +1,27 @@
+import { StyleSheet, View } from 'react-native'
+
+import CircleButton from '../../components/CircleButton'
+import Header from '../../components/Header'
+import Memos from '../../components/Memos'
+
+const List = (): JSX.Element => {
+  return (
+    <View style={styles.container}>
+      <Header />
+
+      <Memos />
+
+      <CircleButton>+</CircleButton>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    // 要素を画面全体に広げる
+    flex: 1,
+    backgroundColor: '#ffffff',
+  },
+})
+
+export default List
